5f5d0f30de617f0368b8facc454d88a0bb18e551
Alex Sobiloff <asobiloff@gmail.com>
2018-09-01 18:09:02 -0700

    feat: add about writing

--- client/components/About.js
+++ client/components/About.js
@@ -3,8 +3,35 @@ import React from 'react';
 const About = () =>{
   return (
     <div id="about">
-      <p>This is an area where I'm going to talk about how great
-        the business is and we kick ass blah blah blah.
+      <p>
+        Birchwood West founder Alex Sobiloff is a software engineer, writer,
+        and photographer. He started Birchwood West to solve two problems. 
+      </p>
+      <p>
+        The first was the tech industy's exclusive attitude toward new
+        engineers. Often times, lesser experienced developers cannot get the
+        attention they deserve regardless of their abilities. The industry's
+        benchmarks for measuring candidate potential is often outdated,
+        inaccurate, and unnecessarily stressful for the candidate. Birchwood
+        West has a different process for discovering talented individuals. We
+        call it "conversation". Birchwood West prioritizes passion,
+        dedication, eagerness to learn, and communication above specific
+        knowledge or skill set. 
+      </p>
+      <p>
+        The second problem was the lack of attention toward small businesses.
+        As the son of a single mother and long time small business owner, Alex
+        started learning from a young age about the fundamental gap that existed 
+        between large and small businesses, as well as the plight that so many 
+        small businesses deal with. The constant juggle between company time 
+        and budget can be an exhausting dance. The fact is, building a web 
+        presence yourself with one of the many online platforms can seem like 
+        a good idea to save money. But inevitably, as time gets tight, the 
+        website is the first sacrifice to be made. The result ends up being a 
+        half finished and never updated site that leaves a small business 
+        looking...well, small. The answer? Deliver a great product at an 
+        affordable price and give small business owners their valuable time 
+        back.
       </p>
     </div>
   )
